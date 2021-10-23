a13ccc0241856f58b166322ba0744daccfbf3b83
kangilbin <kib3388@naver.com>
2021-10-23 19:19:42 +0900

    사진 촬영 함수 생성

--- app/src/main/java/com/kang/study/MainActivity.kt
+++ app/src/main/java/com/kang/study/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.kang.study
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sub.*
+import java.io.File
+import java.io.IOException
+import java.security.Permission
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1 // 카메라 사진 촬영 요청 코드, 신호 값을 확인하기 위해 임의로 설정
-    lateinit var  cuPhotoPath: String // 문자열 형태의 사진 정보 값
+    lateinit var  curPhotoPath: String // 문자열 형태의 사진 정보 값
     //lateinit 은 초기 값을 null로 하고 싶을 때
     var UserList = arrayListOf<User>(
         User(R.drawable.android, "제이콥이네","29","안녕하세요."),
@@ -63,9 +78,65 @@ class MainActivity : AppCompatActivity() {
         }
         //저장된 데이터 로드
         loadData()
-
-
+        //권한 설정 메서드
+        setPermission()
+        btn_camera.setOnClickListener {
+            takeCapture() // 기본 카메라 앱을 실행하여 사진 촬영
+        }
     }
+    // 카메라 촬영
+    private fun takeCapture() {
+        //기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex : IOException){
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile( //보안 관련 코드
+                        this,
+                        "com.kang.study.fileprovider", // 앱에 대한 인증, (패키지 경로.fileprovider) 입력
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    //  메인 액티비티에서 서버 액티비티로 이동 후 다시 메인으로 돌아오면서 받게 되는 결과 값을 받을 수 있다.
+                    // 카메라 또한, 액티비티 이므로 사진을 찍고 다시 결과값을 가지고 와야한다.
+                }
+            }
+        }
+    }
+    // 이미지 파일 생성
+    private fun createImageFile(): File {
+        //날짜 형태로 이름을 설정 변수 생성
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)// Environment(환경)
+        return File.createTempFile("JPEG_${timestamp}_",".jpg",storageDir)
+            .apply { curPhotoPath = absolutePath } // 현재 사진의 경로 넣기, absolutePath -> 절대 경로
+    }
+
+    //권한 만들기
+    private fun setPermission() {
+        val permission = object : PermissionListener{
+            override fun onPermissionGranted() { // 설정한 위험 권한들이 허용 되었을 때 수행
+                Toast.makeText(this@MainActivity, "권한이 허용 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            // 설정한 위험 권한들 중 거부를 한 경우 수행
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@MainActivity, "권한이 거부 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("카메라 앱을 사용하시려면 권한을  허용해 주세요.") //처음 안내 메세지
+            .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해 주세요.") // 사용자가 거부 했을 경우 나타나는 안내 메세지
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA) //카메라 권한과 저장 권한 적용
+            .check() // 권한 체크
+    }
+
     // edit text 저장된 값 불러오기
     private fun loadData() {
         val pref = getSharedPreferences("pref",0)
@@ -83,5 +154,30 @@ class MainActivity : AppCompatActivity() {
         val edit = pref.edit()  //수정 모드
         edit.putString("name", et_id.text.toString()) //첫 번째 인자에는 key값, 두 번재 인자에는 value
         edit.apply() // 저장
+    }
+    //startActivityForResult를 통해서 기본 카메라 앱으로 부터 받은 사진 결과 값
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 이미지를 성공적으로 가져왔으면 시행, requestCode를 처음 지정한 임의의 코드오 같다면
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            val bitmap: Bitmap
+            val file = File(curPhotoPath) // curPhotoPath 사진이 저장된 경로
+            if(Build.VERSION.SDK_INT < 28){ // 안드로이드 9.8(Pie) 버전보다 낮을 경우
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+                iv_list_profile.setImageBitmap(bitmap)
+            } else {
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+                iv_list_profile.setImageBitmap(bitmap)
+            }
+            savePhoto(bitmap)
+        }
+
+    }
+    private fun savePhoto(bitmap: Bitmap) {
+
     }
 }
